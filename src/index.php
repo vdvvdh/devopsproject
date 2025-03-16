ad9a758ff38fab6c1cc99fcd79e1a5f20411b421
vdvvdh <anxin.xie@student.rocva.nl>
2025-03-16 06:34:09 +0100

    back

--- src/index.php
+++ src/index.php
@@ -1,30 +1,20 @@
 <?php
-echo "Hello from the docker container";
+
+echo "Hello from the docker yooooo container";
 
 $mysqli = new mysqli("db", "root", "example", "company1");
 
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-
-$sql = "CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    fav_color VARCHAR(255) NOT NULL
-)";
-$mysqli->query($sql);
-
 $sql = "INSERT INTO users (name, fav_color) VALUES('Lil Sneazy', 'Yellow')";
-$mysqli->query($sql);
+$result = $mysqli->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Nick Jonas', 'Brown')";
-$mysqli->query($sql);
+$result = $mysqli->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Maroon 5', 'Maroon')";
-$mysqli->query($sql);
+$result = $mysqli->query($sql);
 $sql = "INSERT INTO users (name, fav_color) VALUES('Tommy Baker', '043A2B')";
-$mysqli->query($sql);
+$result = $mysqli->query($sql);
+
 
 $sql = 'SELECT * FROM users';
-$users = [];
 
 if ($result = $mysqli->query($sql)) {
     while ($data = $result->fetch_object()) {
@@ -37,6 +27,3 @@ foreach ($users as $user) {
     echo $user->name . " " . $user->fav_color;
     echo "<br>";
 }
-
-$mysqli->close();
-?>
