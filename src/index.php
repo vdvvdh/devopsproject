614fdcc9809d1d8124d44fe1af358c16b737853e
vdvvdh <anxin.xie@student.rocva.nl>
2025-03-16 05:56:37 +0100

    php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,29 @@
 <?php
 
-echo "is it still wroking";
+echo "Hello from the docker container";
 
-?>
+$mysqli = new mysqli("db", "root", "example", "company1");
+
+$sql = "INSERT INTO users (name, fav_color) VALUES('Lil Sneazy', 'Yellow')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('Nick Jonas', 'Brown')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('Maroon 5', 'Maroon')";
+$result = $mysqli->query($sql);
+$sql = "INSERT INTO users (name, fav_color) VALUES('Tommy Baker', '043A2B')";
+$result = $mysqli->query($sql);
+
+
+$sql = 'SELECT * FROM users';
+
+if ($result = $mysqli->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->name . " " . $user->fav_color;
+    echo "<br>";
+}
